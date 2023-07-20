d2e6ab119a56105dfe859529ee11fbacb124e3bd
Masibo <victor.wafula@student.moringaschool.com>
2023-07-20 11:09:15 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+  .then(function(response){
+    return response.json()
+})
+  .then(data => renderBooks(data));
 }
+
+fetchBooks();
 
 function renderBooks(books) {
   const main = document.querySelector('main');
